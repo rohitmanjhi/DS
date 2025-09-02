e5f8e12cd49b51bfc1126335d64394a309aa6715
rohitmanjhi96 <rohitmanjhibtc.com>
2025-09-03 01:15:27 +0530

    two sum logic added

---
+++ ArrayAndHashing/two_sum.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return []
+
+nums = [2,7,11,15]
+target = 9
+print(two_sum(nums, target))
